ab9303b4de99ec14bb692e7736b8729b121e1991
teregiray <dantonengier@mail.ru>
2023-04-04 10:56:30 +0300

    add funcs

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 import express from "express";
+import config from "config";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(5000, () => console.log('App has been started...'));
+const PORT = config.get('port') || 5000;
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true
+        })
+    } catch (e) {
+        console.log("Server Error", e.message)
+        process.exit(1)
+    }
+}
+
+start()
+
+app.listen(5000, () => console.log(`App has been started on port ${PORT}...`));
