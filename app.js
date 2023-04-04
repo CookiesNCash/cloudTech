cb61b331ca09d3fb891de87c883aa25b6a45ca44
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-04-04 11:19:53 +0300

    add mongodb

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ const PORT = config.get('port') || 5000;
 async function start() {
     try {
         await mongoose.connect(config.get('mongoUri'), {
-            useNewUrlParser: true
+            useNewUrlParser: true,
+            useUnifedTopology: true,
+            useCreateIndex: true
         })
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
     } catch (e) {
         console.log("Server Error", e.message)
         process.exit(1)
@@ -18,5 +21,3 @@ async function start() {
 }
 
 start()
-
-app.listen(5000, () => console.log(`App has been started on port ${PORT}...`));
